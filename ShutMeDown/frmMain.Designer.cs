3a5d7d5df24b070a89a617fc1c15c87515f8613f
hookjordan <jordan@jvprogramming.com>
2017-01-20 23:53:14 -0500

    Add project files.

---
+++ ShutMeDown/frmMain.Designer.cs
@@ -0,0 +1,174 @@
+﻿namespace ShutMeDown
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tmr = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numTime = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblRemain = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numTime)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(14, 118);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(87, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(245, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tmr
+            // 
+            this.tmr.Interval = 1000;
+            this.tmr.Tick += new System.EventHandler(this.tmr_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.numTime);
+            this.groupBox1.Location = new System.Drawing.Point(14, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(318, 100);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Settings";
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(7, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(304, 55);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "The amount of minutes to wait before your system shuts down.";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Minutes:";
+            // 
+            // numTime
+            // 
+            this.numTime.Location = new System.Drawing.Point(171, 19);
+            this.numTime.Maximum = new decimal(new int[] {
+            276447231,
+            23283,
+            0,
+            0});
+            this.numTime.Name = "numTime";
+            this.numTime.Size = new System.Drawing.Size(140, 21);
+            this.numTime.TabIndex = 3;
+            this.numTime.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numTime.Value = new decimal(new int[] {
+            120,
+            0,
+            0,
+            0});
+            this.numTime.KeyDown += new System.Windows.Forms.KeyEventHandler(this.numTime_KeyDown);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(104, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Remaining Time:";
+            // 
+            // lblRemain
+            // 
+            this.lblRemain.Font = new System.Drawing.Font("Verdana", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRemain.Location = new System.Drawing.Point(10, 157);
+            this.lblRemain.Name = "lblRemain";
+            this.lblRemain.Size = new System.Drawing.Size(322, 49);
+            this.lblRemain.TabIndex = 4;
+            this.lblRemain.Text = "00:00 (MM:SS)";
+            this.lblRemain.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(346, 215);
+            this.Controls.Add(this.lblRemain);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ShutMeDown :: JordanHook.com";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numTime)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer tmr;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numTime;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblRemain;
+    }
+}
+
---
+++ ShutMeDown/frmMain.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShutMeDown
+{
+    public partial class frmMain : Form
+    {
+        // Counters 
+        public int totalSeconds = 0;
+        public int timePassed = 0; 
+        public frmMain()
+        {
+            InitializeComponent();
+            button2.Enabled = false; 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // start button 
+
+            // dummy check 
+            if(numTime.Value <= 0)
+            {
+                MessageBox.Show("Invalid time, please enter a time greater then 0"); 
+            }
+            else
+            {
+                // valid time to count down
+                // convert to minutes to seconds to ms 
+                timePassed = 0; 
+                totalSeconds = (int)numTime.Value * (1000 * 60);
+
+                // start timer 
+                tmr.Start();
+
+                // swap buttons 
+                button1.Enabled = false;
+                button2.Enabled = true; 
+            }
+        }
+
+        private void tmr_Tick(object sender, EventArgs e)
+        {
+            // incrase by 1 second -> 1000 ms 
+            timePassed += 1000;
+
+            // calculate time left for label 
+            int minLeft = ((totalSeconds - timePassed) / 1000) / 60;
+            int secLeft = ((totalSeconds - timePassed)  / 1000) % 60;
+
+            // update lable text 
+            lblRemain.Text = string.Format("{0:00}:{1:00} (MM:SS)", minLeft, secLeft); 
+
+            // If time is up...   
+            if(minLeft == 0 && secLeft == 0)
+            {
+                // send forced shutdown command 
+                Process.Start("shutdown", "/s /f /t 0");
+
+                // stop timer to stop command from being sent multiple times 
+                tmr.Stop(); 
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // cancel button 
+
+            // cancel timer 
+            tmr.Stop();
+
+            // swap buttons 
+            button1.Enabled = true;
+            button2.Enabled = false; 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void numTime_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                button1.PerformClick(); 
+            }
+        }
+    }
+}
